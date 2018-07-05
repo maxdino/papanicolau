2be87115276a31be0bd36457e0d25dfd1d4ff6b0
maxdino <maxdino_94@hotmail.com>
2018-07-05 00:00:22 -0500

    validando reporte rango

--- application/models/Reportes_papr_m.php
+++ application/models/Reportes_papr_m.php
@@ -25,7 +25,13 @@ public function mostrar_annio()
 
  public function validar_mes($id,$id_mes)
   {
-   return $r = $this->db->query("(SELECT id_mesr,year(fecha_rechazo) as annio,month(fecha_rechazo) as mes from datos WHERE fecha_rechazo!=''  and  year(fecha_rechazo)=".$id." and month(fecha_rechazo)>".$id_mes." GROUP BY id_mesr  ) UNION (SELECT id_mesr,year(fecha_resultado) as annio,month(fecha_resultado) as mes from datos WHERE fecha_resultado!='' and  year(fecha_resultado)=".$id." and month(fecha_resultado)>".$id_mes." GROUP BY id_mesr  ) ORDER BY mes asc")->result();
+    $mes_nombre = array("01" =>"ENERO","02" => "FEBRERO","03" => "MARZO","04" => "ABRIL","05" =>"MAYO","06" => "JUNIO","07" => "JULIO","08" => "AGOSTO","09" =>"SETIEMBRE","10" => "OCTUBRE","11" => "NOVIEMBRE","12" => "DICIEMBRE");
+      foreach ($mes_nombre as $key_1=> $n_mes) {
+        if ($id_mes==$key_1) {
+          $id_mes=$key_1;
+        }
+      } 
+   return $r = $this->db->query("(SELECT id_mesr,year(fecha_rechazo) as annio,month(fecha_rechazo) as mes from datos WHERE fecha_rechazo!=''  and  year(fecha_rechazo)=".$id." and id_mesr>".$id.$id_mes." GROUP BY id_mesr  ) UNION (SELECT id_mesr,year(fecha_resultado) as annio,month(fecha_resultado) as mes from datos WHERE fecha_resultado!='' and  year(fecha_resultado)=".$id." and id_mesr>".$id.$id_mes." GROUP BY id_mesr  ) ORDER BY mes asc")->result();
  } 
 
  }
